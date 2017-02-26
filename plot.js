fb6a1d8142ee81543cc3e3e189f95b6a4aa23fac
Vyacheslav Volodin <v.volodin@gay.ru>
2017-02-26 11:37:36 +0300

    update

--- plot.js
+++ plot.js
@@ -350,5 +350,10 @@ AmCharts.makeChart("chartdiv", {
   "likes": 48,
   "dislikes": 37,
   "time": "2017-02-26 11:36"
+},
+{
+  "likes": 48,
+  "dislikes": 37,
+  "time": "2017-02-26 11:37"
 }]
 });
