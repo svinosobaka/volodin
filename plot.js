ce076f1c21e9ec221e6e3cdd7f61fa22c9d463a8
Vyacheslav Volodin <v.volodin@gay.ru>
2017-02-26 03:23:33 +0300

    update

--- plot.js
+++ plot.js
@@ -42,73 +42,8 @@ AmCharts.makeChart("chartdiv", {
  		"text": "Chart Title"
  	}],
  	"dataProvider":[{
-  "likes": 267,
-  "dislikes": 236,
-  "time": "2017-02-26 03:08"
-},
-{
-  "likes": 267,
-  "dislikes": 236,
-  "time": "2017-02-26 03:10"
-},
-{
-  "likes": 267,
-  "dislikes": 236,
-  "time": "2017-02-26 03:11"
-},
-{
-  "likes": 267,
-  "dislikes": 236,
-  "time": "2017-02-26 03:12"
-},
-{
-  "likes": 267,
-  "dislikes": 236,
-  "time": "2017-02-26 03:13"
-},
-{
-  "likes": 267,
-  "dislikes": 236,
-  "time": "2017-02-26 03:14"
-},
-{
-  "likes": 267,
-  "dislikes": 236,
-  "time": "2017-02-26 03:15"
-},
-{
-  "likes": 267,
-  "dislikes": 236,
-  "time": "2017-02-26 03:16"
-},
-{
-  "likes": 267,
-  "dislikes": 236,
-  "time": "2017-02-26 03:17"
-},
-{
-  "likes": 270,
-  "dislikes": 238,
-  "time": "2017-02-26 03:18"
-},
-{
-  "likes": 270,
-  "dislikes": 238,
-  "time": "2017-02-26 03:19"
-},
-{
-  "likes": 270,
-  "dislikes": 238,
-  "time": "2017-02-26 03:20"
-},
-{
-  "likes": 270,
-  "dislikes": 238,
-  "time": "2017-02-26 03:22"
-},
-{
   "likes": 31,
   "dislikes": 16,
-  "time": "2017-02-26 03:22"
+  "time": "2017-02-26 03:23"
 }]
 });
